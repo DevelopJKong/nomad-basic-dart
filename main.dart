648843252c6d41ec0dee739653389ee43eb69092
cafe small house <ybaram40@gmail.com>
2023-01-12 07:50:58 +0900

    #1.2 Dynamic Type 완료

--- main.dart
+++ main.dart
@@ -1,4 +1,18 @@
 void main() {
   String name = '정빈';
   print(name);
+
+  dynamic username;
+
+  /* username = "정빈빈";
+  username = 12;
+  username = true; */
+
+  if (username is String) {
+    username.length;
+  }
+
+  if (username is int) {
+    username.abs();
+  }
 }
